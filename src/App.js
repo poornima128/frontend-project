b68ba1b16be5d52b781b96c08fc8860e4a4a5ce0
poornima128 <poornimakannan08@gmail.com>
2024-03-09 04:32:39 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Navbar';
+import { Routes, Route } from "react-router-dom";
+import Home from './Home';
+import GetList from './GetList.js';
+import SingleGet from './SingleGet';
+import Update from './Update';
+import Post from './Post';
+
+function App() {
+  return (
+    <div >
+      <Navbar />
+      <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/add" element={<Post />} />
+            <Route path="/get" element={<GetList />} />
+            <Route path="/single-get/:id" element={<SingleGet />} />
+            <Route path="/update/:id" element={<Update />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://www.google.com/search?q=travel+liternary+planner+image&oq=&gs_lcrp=EgZjaHJvbWUqCQgAEEUYOxjCAzIJCAAQRRg7GMIDMgkIARBFGDsYwgMyCQgCEEUYOxjCAzIRCAMQABgDGEIYjwEYtAIY6gIyEQgEEAAYAxhCGI8BGLQCGOoCMg8IBRAuGAMYjwEYtAIY6gIyEQgGEAAYAxhCGI8BGLQCGOoCMhEIBxAAGAMYQhiPARi0AhjqAtIBCDc3OGowajE1qAIIsAIB&sourceid=chrome&ie=UTF-8&safe=active&ssui=on#vhid=e7mVQO3Gs02xcM&vssid=l" alt="Welcome picture" />
+    </div>
+  )
+}
